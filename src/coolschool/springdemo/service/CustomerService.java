c30ccec2ba0416ee56be036a77c7a9eee46e85b3
rosicaz <rosicaz@abv.bg>
2019-10-27 18:44:23 +0200

    initial commit

---
+++ src/coolschool/springdemo/service/CustomerService.java
@@ -0,0 +1,16 @@
+package coolschool.springdemo.service;
+
+import java.util.List;
+
+import coolschool.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer>  getCustomer() ;
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+}
